e20e99f557db1ef633f5ce3d601754404cc4aded
George <george@gmail.com>
2020-02-29 19:14:36 -0500

    adding multiple cards

--- src/App.js
+++ src/App.js
@@ -2,11 +2,32 @@ import React, { useEffect } from "react";
 // import "./styles-cards.css";
 import CardComponent from "./components/CardComponent/CardComponent";
 import data from "./data/quotes.json";
+import styled from "styled-components";
+
+const Div = styled.div`
+  @media (max-width: 500px) {
+    grid-template-columns: 0px 100% 0px;
+
+    .card {
+      width: 80% !important;
+      /* width: 80vw !important; */
+      height: 50vh !important;
+      /* max-height: 50vh; */
+      margin: 0;
+      bottom: 30vh;
+
+      #text {
+        line-height: 25px;
+        font-size: 1.2rem;
+      }
+    }
+  }
+
+  @media (max-width: 800px) {
+  }
+`;
 
 function App() {
-  // const data = useState(null);
-  console.log("Json lenght is: ", data.length, data[0]);
-
   let cards = [];
 
   //Load 10 random cards from JSON "data";
@@ -15,23 +36,24 @@ function App() {
     cards.push(data[Math.floor(Math.random() * data.length)]);
   }
 
-  console.log(cards);
-
   useEffect(() => {
     // data =
   }, []);
 
   return (
-    <div className="wrapper" id="quote-box">
+    <Div className="wrapper" id="quote-box">
       <div className="header">
         <div className="pause" />
-        {/* <div href='#' class="theme"></div> */}
       </div>
       <div className="cards">
-        <CardComponent
-          text={cards[0].quoteText}
-          author={cards[0].quoteAuthor}
-        />
+        {cards.map((card, i) => (
+          <CardComponent
+            key={i}
+            text={cards[i].quoteText}
+            author={cards[i].quoteAuthor}
+            zIndex={i + 1}
+          />
+        ))}
       </div>
       <div className="footer">
         <div href="http://facebook.com" className="facebook" />
@@ -39,7 +61,7 @@ function App() {
         <div href="http://facebook.com" id="tweet-quote" className="twitter" />
         <div className="NEXT" id="new-quote" />
       </div>
-    </div>
+    </Div>
   );
 }
 
--- src/components/CardComponent/CardComponent.jsx
+++ src/components/CardComponent/CardComponent.jsx
@@ -1,49 +1,42 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Draggable } from "gsap/all";
 import gsap from "gsap";
 import styled from "styled-components";
 
-const Div = styled.div`
-  @media (max-width: 500px) {
-    /* border: 2px solid red !important; */
-    width: 80vw !important;
-    height: auto !important;
-    max-height: 60vh;
-
-    #text {
-      font-size: 1.2rem;
-    }
-  }
-`;
-
 const CardComponent = props => {
+  const Div = styled.div`
+    z-index: ${props.zIndex};
+  `;
+
   const [show, setShow] = useState(true);
+  const cardRef = useRef(null);
+
   useEffect(() => {
     gsap.registerPlugin(Draggable);
-    Draggable.create(".draggable", {
+    Draggable.create(cardRef.current, {
       type: "x,y",
       onPress: function() {
         //  alert("clicked");
       },
       onDrag: function() {
-        gsap.to(".draggable", 0.8, {
+        gsap.to(cardRef.current, 0.8, {
           //   tranformOrigin: "bottom 50%",
           rotationZ: this.deltaX * 1.2 + this.deltaY * 1,
           rotationX: this.deltaY * 0
         });
       },
       onRelease: function() {
-        if (this.endX < window.innerWidth / 3) {
+        if (this.endX < window.innerWidth / 5) {
           //   gsap.to(".draggable", 0.4, {
           //     tranformOrigin: "top left",
           //     rotationX: 0,
           //     rotationY: 0,
           //     rotationZ: 0
           //   });
-          gsap.to(".draggable", 0.8, { x: 0, y: 0, rotation: 0 });
+          gsap.to(cardRef.current, 0.8, { x: 0, y: 0, rotation: 0 });
         } else {
           gsap
-            .to(".draggable", 3.4, { x: 4500 })
+            .to(cardRef.current, 3.4, { x: 4500 })
             .eventCallback("onComplete", () => setShow(false));
         }
       }
@@ -51,21 +44,22 @@ const CardComponent = props => {
   }, []);
 
   const handleMouseOver = e => {
-    gsap.to(".draggable", 0.2, {
+    gsap.to(cardRef.current, 0.2, {
       scale: 1.1,
       transformPerspective: 800,
       rotationX: "-8deg",
       boxShadow: "15px 15px 10px 1px rgba(94, 94, 94, 0.8)"
     });
-    gsap.to(".draggable", 0.8, {
+    gsap.to(cardRef.current, 0.8, {
       rotationX: 0,
       delay: 0.3
     });
   };
 
   const handleMouseLeave = e => {
-    gsap.to(".draggable", 0.2, {
-      scale: 0.91,
+    gsap.to(cardRef.current, 0.2, {
+      // scale: 0.91,
+      scale: 1.0,
       rotationX: 0,
       rotationZ: 0,
       boxShadow: "5px 5px 5px 1px rgba(94, 94, 94, 0.8)"
@@ -79,6 +73,7 @@ const CardComponent = props => {
         className="card draggable"
         onMouseOver={handleMouseOver}
         onMouseLeave={handleMouseLeave}
+        ref={cardRef}
       >
         <div className="quote" id="text">
           {/* We can only learn to love by loving. */}
